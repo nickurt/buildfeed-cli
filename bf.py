71450b94f33ecbfab6cbaaaa7b5a7a1e120ab409
nickurt <nickurt@users.noreply.github.com>
2015-05-22 21:33:23 +0200

    Cleaned up the code ;>

--- bf.py
+++ bf.py
@@ -5,37 +5,24 @@ import argparse, feedparser
 def fetchData(url):
 	return feedparser.parse(url)
 
-def latest(args):
-	d = fetchData('https://buildfeed.net/rss/')
+def getData(args):
+	types = { 
+		'latest'	:'', 
+		'leaked'	:'leaked',
+		'added'		:'added',
+		'highest'	:'version',
+		'flights'	:'flight'
+	}
 
-	for entries in d.entries:
-		print entries.title
+	risk = '';
 
-def leaked(args):
-	d = fetchData('https://buildfeed.net/rss/leaked/')
-
-	for entries in d.entries:
-		print entries.title
-
-def added(args):
-	d = fetchData('https://buildfeed.net/rss/added/')
-
-	for entries in d.entries:
-		print entries.title
-
-def highest(args):
-	d = fetchData('https://buildfeed.net/rss/version/')
-
-	for entries in d.entries:
-		print entries.title
-
-def flights(args):
 	availableFlights = ['low', 'medium', 'high']
 
-	if args.risk not in availableFlights:
-		args.risk = 'high'
+	if args.type is 'flights':
+		risk = args.risk if args.risk in availableFlights else 'high'
 
-	d = fetchData('https://buildfeed.net/rss/flight/' + args.risk)
+	# Fetch the data
+	d = fetchData('https://buildfeed.net/rss/' + types[args.type] + '/' + risk)
 
 	for entries in d.entries:
 		print entries.title
@@ -44,29 +31,29 @@ if __name__ == '__main__':
 
 	# Parser
 	parser = argparse.ArgumentParser(prog='BuildFeed')
-	parser.add_argument('--version', action='version', version='%(prog)s 0.1')
+	parser.add_argument('--version', action='version', version='%(prog)s 0.1.1')
 	subparsers = parser.add_subparsers()
 
 	# Latest
 	latest_p = subparsers.add_parser('latest')
-	latest_p.set_defaults(func=latest)
+	latest_p.set_defaults(func=getData, type='latest')
 
 	# Leaked
 	latest_p = subparsers.add_parser('leaked')
-	latest_p.set_defaults(func=leaked)
+	latest_p.set_defaults(func=getData, type='leaked')
 
 	# Added
 	added_p = subparsers.add_parser('added')
-	added_p.set_defaults(func=added)
+	added_p.set_defaults(func=getData, type='added')
 
 	# Highest
 	highest_p = subparsers.add_parser('highest')
-	highest_p.set_defaults(func=highest)
+	highest_p.set_defaults(func=getData, type='highest')
 
 	# Flights
 	flights_p = subparsers.add_parser('flights')
 	flights_p.add_argument('-r', '--risk', default='high', dest='risk')
-	flights_p.set_defaults(func=flights)
+	flights_p.set_defaults(func=getData, type='flights')
 
 	# Args
 	args = parser.parse_args()
